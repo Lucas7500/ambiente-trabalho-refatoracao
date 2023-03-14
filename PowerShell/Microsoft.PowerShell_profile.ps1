57ca65e87f39a831875664c003890d6ef22b036f
dev-macb <dev.macb@gmail.com>
2023-03-14 00:29:03 -0300

    Meus Arquivos

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,96 @@
+# Declaração de Alias
+New-Alias v nvim
+New-Alias e explorer
+
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "C:\Users\miguel\.config\starship.toml"
+
+function dev {
+    param([string]$local)
+    Switch ($local) {
+        "web"    { Set-Location -Path "C:/xampp/htdocs" }
+        "macb"   { Set-Location -Path "$HOME/Projetos/MacB" }
+        "miguel" { Set-Location -Path "$HOME/Projetos/Miguel" }
+        "mirage" { Set-Location -Path "$HOME/Projetos/Mirage" }
+        default  { Set-Location -Path "$HOME/Projetos" }
+    }
+    Get-ChildItem
+}
+function xx {
+    param([Parameter(Mandatory=$true)] [string]$caminho)
+    try {
+        Remove-Item -Path $caminho -Recurse -Force
+        imprimir_msg "Pasta/Arquivo excluida(o) com êxito!" -nova_linha 1
+    }
+    catch {
+        imprimir_msg "Erro ao excluir pasta!" -nova_linha 1
+    }
+}
+
+function novo {
+    param([string]$nome, [string]$conteudo)
+    try {
+        New-Item -ItemType File $nome
+        Set-Content -Path $nome -Value $conteudo
+    }
+    catch {
+        Write-Host "NOVO ARQUIVO"
+        Write-Host ""
+        Write-Host "Uso: novo <nome_arquivo> <conteudo>"
+        Write-Host "     novo -nome '<nome_arquivo>' -conteudo '<conteudo>'"
+        Write-Host ""
+        Write-Host "-nome           O nome do arquivo junto com sua extenção."
+        Write-Host "-conteudo       O conteúdo do arquivo."
+        Write-Host ""
+        Write-Host "Exemplo: novo -nome 'nome do arquivo.txt' -conteudo 'Olá Mundo'"
+    }
+}
+
+function gg() {
+    param([string]$mensagem)
+    try {
+        Write-Host ""
+        Write-Host "COMMIT RÁPIDO" -ForegroundColor Green
+        Write-Host "------ ------" -ForegroundColor Green
+        imprimir_msg "Status atual do repositório: " -nova_linha 1
+        git status -s
+        Write-Host ""
+        imprimir_msg "Adicionando todos os arquivos alterados para o commit... " -nova_linha 0
+        git add .
+        imprimir_ok
+        imprimir_msg "Realizando o commit... "  -nova_linha 1
+        git commit -m $mensagem
+        Write-Host "    ✅ Commit realizado!" -ForegroundColor Green
+        Write-Host ""
+        imprimir_msg "Último histórico de commit realizado: "  -nova_linha 1
+        git log -1
+        Write-Host ""
+        imprimir_msg "Enviando conteúdo local para o repositório remoto... " -nova_linha 1
+        git push
+        Write-Host "    ✅ Arquivos enviados!" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "COMMIT RÁPIDO" -ForegroundColor Gray
+        Write-Host ""
+        Write-Host "Uso: gg <mensagem>"
+        Write-Host "     novo -mensagem '<mensagem>'"
+        Write-Host ""
+        Write-Host "-mensagem       A comentário do commit."
+        Write-Host ""
+        Write-Host "Exemplo: novo -nome 'nome do arquivo.txt' -conteudo 'Olá Mundo'"
+    }
+}
+
+
+function imprimir_msg {
+    param([string]$mensagem, [bool]$nova_linha)
+    Write-Host "[" -ForegroundColor Gray -NoNewline;
+    Write-Host "*" -ForegroundColor Green -NoNewline;
+    Write-Host "] $mensagem" -ForegroundColor Gray -NoNewline;
+    if ($nova_linha) { Write-Host "" }
+}
+
+function imprimir_ok {
+    Write-Host "OK" -ForegroundColor Green;
+    Write-Host ""
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# ambiente-trabalho
-🎨 Configurações do meu ambiente de desenvolvimento.
+<h1 align="center">🔷 Meu Ambiente de Trabalho 🔷</h1>
+
+
+<h2 id="objetivo">🎯 Objetivo</h2>
+<p>
+    Este repositório contém os arquivos de configuração do meu ambiente de 
+    desenvolvimento pessoal, incluindo arquivos de configuração para diversas 
+    ferramentas que utilizo, como meu editor de código, terminal, shells, e 
+    outras configurações personalizadas que aprimoram minha produtividade no 
+    dia-a-dia. Manter esses arquivos em um repositório me permite sincronizá-los 
+    facilmente em diferentes dispositivos e garantir que minhas configurações 
+    permaneçam consistentes em todas as máquinas em que trabalho. Sinta-se 
+    livre para utilizar esses arquivos como base para suas próprias configurações 
+    pessoais de desenvolvimento.
+</p>
+<p align="center">🔷</p>
+
+
+<h2 id="instalação">🔧 Instalação</h2>
+<p>
+    Para clonar o projeto para sua máquina via <a target="_blank" href="https://git-scm.com/">git</a>, execute os comandos a seguir:
+</p>
+
+```bash
+$ mkdir braz && cd braz
+$ git clone https://github.com/dev-macb/braz
+```
+<p align="center">🔷</p>
+
+
+<h2 id="licença">📄 Licença</h2>
+<p>
+    O repositório <strong>AmbienteTrabalho</strong> utiliza a <strong>licença MIT</strong> em todo seu código, confira suas condições em <a href="https://github.com/Miguel-acb9/ambiente-trabalho/blob/main/LICENSE">LICENSE</a>.
+</p>
+<p align="center">🔷</p>
